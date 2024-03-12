330d6494fc930531b82827ace6e2ef20fd344070
Bagas Arya Pradipta <bagasarya150800@gmail.com>
2024-03-12 22:44:01 +0700

    update category module : add fn select category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
@@ -7,14 +16,30 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
+  @Get('select-option/:params/:userId')
+  async selectCategory(
+    @Param('params') params: string,
+    @Param('userId') userId: string,
+  ) {
+    return this.categoryService.selectCategory(params, userId);
+  }
+
   @Post()
   create(@Body() body: CreateCategoryDto) {
     return this.categoryService.create(body);
   }
 
   @Post('get-category')
-  findAll(@Body() body: { page: number; limit: number, search:string, user_id:string }) {
-    console.log(body, 'INI CEK PAGE LIMIT')
+  findAll(
+    @Body()
+    body: {
+      page: number;
+      limit: number;
+      search: string;
+      user_id: string;
+    },
+  ) {
+    console.log(body, 'INI CEK PAGE LIMIT');
     return this.categoryService.findAll(body);
   }
 
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -8,62 +8,90 @@ import { Op } from 'sequelize';
 
 @Injectable()
 export class CategoryService {
-  async create(body: CreateCategoryDto) {
-    try{
-      const myUUID = uuidv4();
-      const { category_name, category_type, user_id  } = body;
+  async selectCategory(params: string, userId: string) {
+    try {
+      const collection = await category.findAll({
+        where: { category_type: params, user_id: userId },
+      });
 
-      const existingCategory = await category.findOne({ where: { category_name, user_id } });
+      if (!collection || collection.length === 0) {
+        return { status: 404, message: 'Category not found' };
+      }
+
+      return { status: 200, message: 'Success', records: collection };
+    } catch (error) {
+      return {
+        status: 500,
+        message: 'Internal server error',
+        error: error.message,
+      };
+    }
+  }
+
+  async create(body: CreateCategoryDto) {
+    try {
+      const myUUID = uuidv4();
+      const { category_name, category_type, user_id } = body;
+
+      const existingCategory = await category.findOne({
+        where: { category_name, user_id },
+      });
 
       if (existingCategory) {
-        throw new HttpException({
-          status: 400,
-          message: 'Category already exist',
-        }, HttpStatus.BAD_REQUEST);
+        throw new HttpException(
+          {
+            status: 400,
+            message: 'Category already exist',
+          },
+          HttpStatus.BAD_REQUEST,
+        );
       }
 
       const data = await category.create({
         id: myUUID,
         category_name: category_name,
-        category_type : category_type,
-        user_id : user_id
-      })
+        category_type: category_type,
+        user_id: user_id,
+      });
 
       return {
         status: 201,
         message: 'Create category successfully',
         result: data,
       };
-    }catch(error){
+    } catch (error) {
       throw error;
     }
   }
 
-  async findAll(body: { page: number, limit: number, search: string, user_id: string }) {
+  async findAll(body: {
+    page: number;
+    limit: number;
+    search: string;
+    user_id: string;
+  }) {
     try {
       const { page, limit, search, user_id } = body;
       const offset = (page - 1) * limit;
       let whereClause: any = { user_id };
-      let orderClause: any = [['created_at', 'DESC']]; 
-  
+      let orderClause: any = [['created_at', 'DESC']];
+
       if (search) {
         whereClause = {
           ...whereClause,
-          [Op.or]: [
-            { category_name: { [Op.like]: `%${search}%` } },
-          ],
+          [Op.or]: [{ category_name: { [Op.like]: `%${search}%` } }],
         };
       }
-  
+
       const data = await category.findAll({
         where: whereClause,
         limit,
         offset,
-        order: orderClause
+        order: orderClause,
       });
-  
+
       const totalCount = await category.count({ where: whereClause });
-  
+
       return responsePaginate(data, totalCount, page, limit);
     } catch (error) {
       throw error;
@@ -72,26 +100,31 @@ export class CategoryService {
 
   async update(body: UpdateCategoryDto) {
     try {
-      const {id, category_name, category_type, user_id } = body;
+      const { id, category_name, category_type, user_id } = body;
 
-      const existingCategory = await category.findOne({ where: { category_name, id: { [Op.not]: id } } });
-  
+      const existingCategory = await category.findOne({
+        where: { category_name, id: { [Op.not]: id } },
+      });
+
       if (existingCategory) {
-        throw new HttpException({
-          status: 400,
-          message: 'Category name already exists for another category',
-        }, HttpStatus.BAD_REQUEST);
+        throw new HttpException(
+          {
+            status: 400,
+            message: 'Category name already exists for another category',
+          },
+          HttpStatus.BAD_REQUEST,
+        );
       }
-  
+
       const updatedCategory = await category.update(
         {
           category_name: category_name,
-          category_type: category_type, 
-          user_id: user_id
+          category_type: category_type,
+          user_id: user_id,
         },
         { where: { id: id }, returning: true },
       );
-  
+
       return {
         status: 200,
         message: 'Category updated successfully',
@@ -101,18 +134,21 @@ export class CategoryService {
       throw error;
     }
   }
-  
-  async remove(id : string) {
+
+  async remove(id: string) {
     try {
       const deletedCategory = await category.destroy({ where: { id } });
-      
+
       if (deletedCategory === 0) {
-        throw new HttpException({
-          status: 404,
-          message: 'Category not found',
-        }, HttpStatus.NOT_FOUND);
+        throw new HttpException(
+          {
+            status: 404,
+            message: 'Category not found',
+          },
+          HttpStatus.NOT_FOUND,
+        );
       }
-  
+
       return {
         status: 200,
         message: 'Category deleted successfully',
