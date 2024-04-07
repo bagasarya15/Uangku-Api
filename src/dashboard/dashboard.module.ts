323adb8f42a1fc86bc777d315e861664a1e82e17
Bagas Arya Pradipta <bagasarya150800@gmail.com>
2024-04-08 05:17:19 +0700

    update dashboard.module

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { expense, income } from '../../models';
 
 @Module({
+  imports: [SequelizeModule.forFeature([income, expense])],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
