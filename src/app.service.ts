fce4f50858b8188e0aa985650dc2d569e9c29b15
Bagas Arya Pradipta <bagasarya150800@gmail.com>
2024-07-22 01:44:24 +0700

    update hello service

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     // return 'Hello World!';
-    return 'SUCCESS';
+    return 'UANGKU API V.1.2';
   }
 }
